3ca6ed4b1e2fa9bafc867ca6c37dbea541e4fb67
Jonas Myrenås <jonas.myrenas@gmail.com>
2025-02-01 22:50:13 +0100

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Provider } from "@/components/ui/provider";
 import { Box, Heading, Image } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import { Form } from "./components/Form";
 
 function App() {
   return (
     <Provider>
+      <Analytics />
       <Box mb={4}>
         <Heading as="h1">Backgammon strike statistics</Heading>
       </Box>
